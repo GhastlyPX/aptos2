f4d358de1be5df1f60a2e709f81998b1835c63f9
GhastlyPX <ghastly.pixels@gmail.com>
2022-10-25 22:46:28 +0100

    aptos launchpad

--- components/Helpers.tsx
+++
@@ -1,2 +0,0 @@
-export const candyMachineAddress = "0x11c6b29900c29b1de6595e28dde341947edf2a612bb3a0bb2b560ab98908c1ac";
-export const collectionName = "Immers3Test";
--- components/candyMachineInfo.tsx
+++ components/candyMachineInfo.tsx
@@ -1,16 +1,19 @@
-export const candyMachineAddress = "0x30243c6480936f276dee057907077fdeb9f31425f158a6e8c296b5df26191db4";
-export const collectionName = "Test"; // Case sensitive!
-export const mode = "dev"; // "dev" or "test" or "mainnet"
+export const candyMachineAddress = "0x33a6189ea0beb66e37953181a0e6afaa65b91e151e4cb99470488c2b095bcd42";
+export const collectionName = "Immers3 x Arlo"; // Case sensitive!
+export const mode = "mainnet"; // "dev" or "test" or "mainnet"
 
 export let NODE_URL: string;
 let FAUCET_URL;
+// @ts-ignore
 if (mode == "dev") {
     NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
     FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
-} else if (mode === "test") {
-    NODE_URL = "https://fullnode.testnet.aptoslabs.com/v1";
-    FAUCET_URL = "https://faucet.testnet.aptoslabs.com";
-} else {
-    NODE_URL = "https://aptos-mainnet.nodereal.io/v1/ae1d16cac902481a983971715430e422";
-    FAUCET_URL = null;
+} else { // @ts-ignore
+    if (mode === "test") {
+        NODE_URL = "https://fullnode.testnet.aptoslabs.com/v1";
+        FAUCET_URL = "https://faucet.testnet.aptoslabs.com";
+    } else {
+        NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
+        FAUCET_URL = null;
+    }
 }
