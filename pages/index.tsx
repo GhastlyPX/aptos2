d94addfc865083ea448d50ac55b21ef489ac9cc3
GhastlyPX <ghastly.pixels@gmail.com>
2022-10-29 15:35:05 +0100

    aptos launchpad

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,7 @@ const Home: NextPage = () => {
                             <div className={"flex flex-col gap-2 items-center"}>
                                 <h1 className={"text-[18px]"}>Minted</h1>
                                 <h1 className={"text-[12px] text-[#B4B4B4]"}>{// @ts-ignore
-                                    candyMachineData?.data?.numUploadedTokens}/4000</h1>
+                                    candyMachineData?.data?.numUploadedTokens}/800</h1>
                             </div>
                         </div>
                         <div className={"flex justify-center text-[16px]"}>
