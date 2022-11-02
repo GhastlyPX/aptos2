022f69a96bb2b47ed07473442773383d3348747d
GhastlyPX <ghastly.pixels@gmail.com>
2022-11-02 17:52:06 +0000

    aptos launchpad

--- components/candyMachineInfo.tsx
+++ components/candyMachineInfo.tsx
@@ -1,10 +1,10 @@
 export const candyMachineAddress = "0xd342674792fd7d24dbba2c81014148b5148e6299a8b4c5a69fb71bc645c08c06";
-export const collectionName = "Anarchy Dog"; // Case sensitive!
+export const collectionName = "Anarchy Dogs"; // Case sensitive!
 export const mode = "mainnet"; // "dev" or "test" or "mainnet"
 export const twitter = "https://twitter.com/dog_anarchy";
 export const start = 1667414700;
 export const supply = 2000;
-export const price = 1.3;
+export const price = 1;
 
 export let NODE_URL: string;
 let FAUCET_URL;
