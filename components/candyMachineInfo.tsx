78126510fc2ec70856df7057a666014e7b6fba24
GhastlyPX <ghastly.pixels@gmail.com>
2022-11-06 16:21:09 +0000

    aptos launchpad

--- components/candyMachineInfo.tsx
+++ components/candyMachineInfo.tsx
@@ -4,7 +4,7 @@ export const mode = "mainnet"; // "dev" or "test" or "mainnet"
 export const twitter = "https://twitter.com/AYTLONFT";
 export const start = 0;
 export const supply = 2222;
-export const price = 0.8;
+export const price = 0.5;
 
 export let NODE_URL: string;
 let FAUCET_URL;
